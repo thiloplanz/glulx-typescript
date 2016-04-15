c90741c95285361d8935ca89b7813363f4df1162
Thilo Planz <thilo@cpan.org>
2016-04-15 11:04:44 +0800

    add an example of embedding EngineWrapper directly in a browser page, without going via a web worker

---
+++ example/web/engineWrapper/engineWrapper.ts
@@ -0,0 +1,114 @@
+// Written in 2016 by Thilo Planz 
+// To the extent possible under law, I have dedicated all copyright and related and neighboring rights 
+// to this software to the public domain worldwide. This software is distributed without any warranty. 
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+/**
+ * A simple demo of embedding EngineWrapper directly
+ * (without going through a web worker) in a web browser
+ */
+
+
+/// <reference path='../../../core/EngineWrapper.ts' />
+
+module Example {
+
+export function loadGameImage(){
+    var file = document.getElementById('gameImage')['files'][0];
+    setText("status", "loading game image");
+    setText('selectFile', '');
+    var reader = new FileReader();
+    reader.onload = function(ev){
+        let arrayBuffer = ev.target['result']
+        let image = new FyreVM.MemoryAccess(0,0)
+        image['buffer'] = new Uint8Array(arrayBuffer);
+		image['maxSize'] = arrayBuffer.byteLength;
+        w = new FyreVM.EngineWrapper(image, false)
+        process(w.run())
+       
+    }
+    reader.readAsArrayBuffer(file);
+        
+}
+
+
+function process(result: FyreVM.EngineWrapperState){
+     let c = result.channelData
+     if (c) {
+         if (c.MAIN){
+            setText('MAIN',c.MAIN);
+        }
+        if (c.LOCN){
+            setText('LOCN', c.LOCN);
+        }
+        if (c.SCOR || +c.SCOR === 0){
+            setText('SCOR', "Score: "+c.SCOR);
+        }
+        if (c.TURN){
+            setText('TURN', "Turn: "+c.TURN);
+        }
+        if (c.TIME){
+            setText('TIME', Math.floor(+c.TIME / 100) +":"+ (+c.TIME % 100));
+        }
+        if (c.PRPT){
+            promptLine = c.PRPT;
+        }
+     }
+    
+    
+     switch(result.state){
+         case FyreVM.EngineState.waitingForKeyInput:
+         case FyreVM.EngineState.waitingForLineInput:
+            setText('status', 'waiting for your input...');
+			getInput();
+			break;
+         case FyreVM.EngineState.completed:
+            setText('status', 'game over');
+			break;
+         case FyreVM.EngineState.waitingForLoadSaveGame:
+         case FyreVM.EngineState.waitingForGameSavedConfirmation:
+            setText('status', "ERROR: saving games not implemented")
+            break;
+         default:
+            setText('status', "ERROR: unexpected Engine state "+result.state)
+            console.error(result);
+            break;
+         
+     }
+}
+
+
+
+
+function setText(id, text){
+    document.getElementById(id).textContent = text
+}
+
+
+let promptLine;
+
+function getInput(){
+    var div = document.getElementById('PRPT');
+    div.innerHTML = '<form onsubmit="Example.sendCommand(); return false;">'+promptLine+' <input size=80></input><form>';
+    var input : any = div.getElementsByTagName('INPUT')[0];
+    input.focus();
+    input.scrollIntoView();	
+}
+
+ // the EngineWrapper, instance created when game image is loaded
+let w : FyreVM.EngineWrapper;
+
+export function sendCommand(){
+    var div = document.getElementById('PRPT');
+    var input : any = div.getElementsByTagName('INPUT')[0];
+    var command = input.value;
+    div.innerHTML = '';
+    setText('status', 'processing ...');
+    setText('MAIN', '\n\n\n... '+command);
+    // kick off the engine wrapped in setTimeout
+    // a) to return from the submit handler quickly
+    // b) to limit recursion depth
+    setTimeout( () => process(w.receiveLine(command)), 0)
+ }
+
+}
